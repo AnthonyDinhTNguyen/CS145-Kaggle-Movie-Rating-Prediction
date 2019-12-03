172e479ac04c5cc62f3828e3da58342eaf52579d
Edward Zhang Lu <edwardzlu98@gmail.com>
2019-12-03 14:23:06 -0800

    Pushed further progress in trying to do movie vector distance based on genre

--- bak_usersKNN.py
+++ bak_usersKNN.py
@@ -16,22 +16,33 @@ def getDistanceUsers(userA, userB, ratings_matrix):
     distance = scipy.spatial.distance.cosine(ratings_matrix.iloc[userA].values.tolist(), ratings_matrix.iloc[userB].values.tolist())
     return distance
 
+print_t("Reading in movies.csv...")
+df_m = pd.read_csv('../movies.csv', sep=',', names=['movieId', 'title', 'genres'], skiprows=1)
+
+print("df_m head:")
+print(df_m.head())
+
+print("df_m describe:")
+print(df_m.describe())
+
+print("Number of unique genre combinations: " + str(df_m['genres'].nunique()))
+
 print_t("Reading in val_ratings_binary.csv...")
-df = pd.read_csv('../val_ratings_binary.csv', sep=',', names=['userId', 'movieId', 'rating'], skiprows=1)
+df_vrb = pd.read_csv('../val_ratings_binary.csv', sep=',', names=['userId', 'movieId', 'rating'], skiprows=1)
 print_t("Done reading in val_ratings_binary.csv.")
 
 print_t("Replacing 0 with -1...")
-df['rating'].replace(0, -1, inplace=True)
+df_vrb['rating'].replace(0, -1, inplace=True)
 
-print("df head:")
-print(df)
+print("df_vrb head:")
+print(df_vrb.head())
 
-print("df describe:")
-print(df.describe())
+print("df_vrb describe:")
+print(df_vrb.describe())
 
-print_t("Pivoting user ratings data...");
-ratings_matrix = df.pivot(index='userId', columns='movieId', values='rating')
-print_t("Done pivoting user ratings data.");
+print_t("Pivoting user ratings data...")
+ratings_matrix = df_vrb.pivot(index='userId', columns='movieId', values='rating')
+print_t("Done pivoting user ratings data.")
 
 print_t("Replacing NaN with 0 in pivoted user data...")
 ratings_matrix.replace(np.nan, 0, inplace=True)
