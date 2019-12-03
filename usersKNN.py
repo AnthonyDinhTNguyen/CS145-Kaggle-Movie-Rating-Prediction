22771449ef4c1ba17874d2e61fbb02ffe475edc4
Edward Zhang Lu <edwardzlu98@gmail.com>
2019-12-03 12:40:03 -0800

    Got a naive user distance implementation working with pandas library, unfortunately it seems like most vectors are 1.0 distance, I assume it's something to do with the curse of high dimensionality

--- usersKNN.py
+++ usersKNN.py
@@ -1,4 +1,5 @@
 
+import scipy.spatial
 import pandas as pd
 import numpy as np
 import warnings
@@ -11,16 +12,28 @@ def cur_time_millis():
 def print_t(msg):
     print(msg + " (time " + str(cur_time_millis()) + ")")
 
+def getDistanceUsers(userA, userB, ratings_matrix):
+    distance = scipy.spatial.distance.cosine(ratings_matrix.iloc[userA].values.tolist(), ratings_matrix.iloc[userB].values.tolist())
+    return distance
+
 print_t("Reading in val_ratings_binary.csv...")
-
-df = pd.read_csv('../val_ratings_binary.csv', sep=',', names=['userId', 'movieId', 'rating'])
-
+df = pd.read_csv('../val_ratings_binary.csv', sep=',', names=['userId', 'movieId', 'rating'], skiprows=1)
 print_t("Done reading in val_ratings_binary.csv.")
 
-print(df.head())
+print_t("Replacing 0 with -1...")
+df['rating'].replace(0, -1, inplace=True)
 
+print("df head:")
+print(df)
+
+print("df describe:")
 print(df.describe())
 
-ratings_matrix = df.pivot_table(index='userId', columns='movieId', values='rating')
+print_t("Pivoting user ratings data...");
+ratings_matrix = df.pivot(index='userId', columns='movieId', values='rating')
+print_t("Done pivoting user ratings data.");
 
-print(ratings_matrix.head())
+print_t("Replacing NaN with 0 in pivoted user data...")
+ratings_matrix.replace(np.nan, 0, inplace=True)
+
+print(getDistanceUsers(0, 0, ratings_matrix))
